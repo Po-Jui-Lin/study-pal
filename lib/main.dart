8231b42480987bd949bf726b4564505c3f7b92f9
wylerwwl <wongwailun84@gmail.com>
2021-05-18 22:23:11 +0800

    add input module

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:study_pal/chat/chatScreens/chat.dart';
 
 void main() {
   runApp(MyApp());
@@ -104,7 +105,13 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        // onPressed: _incrementCounter,
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Chat(),
+          ),
+        ),
         tooltip: 'Increment',
         child: Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
