cacbbabff4d3c74945909c5e4e61c0a1f18658ca
wylerwwl <wongwailun84@gmail.com>
2021-05-19 13:35:01 +0800

    add text msg bubble

---
+++ lib/chat/chatScreens/message.dart
@@ -0,0 +1,57 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:study_pal/chat/chatScreens/multimedia_message/text_msg.dart';
+
+Widget message(BuildContext context, int index, DocumentSnapshot document, String userId, String peerId) {
+  // print(document.get("content"));
+  if (document.get('idFrom') == userId) {
+    // Right (my message)
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            // Text("hello"),
+            // Text
+            document.get('type') == 0
+                ? textMsg(document, true)
+                // ? Text("hello")
+                // Image
+                // : document.data()['type'] == 1
+                //     ? photoMsg(document, context)
+                //     // file
+                //     : document.data()['type'] == 2
+                //         ? fileMsg(document)
+                : Container(), // dummy else statement for error message type
+          ],
+          mainAxisAlignment: MainAxisAlignment.end,
+        ),
+        // timestamp(document, true),
+      ],
+    );
+  } else {
+    // Left (peer message)
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              // Text("hello"),
+              // text message
+              document.get('type') == 0
+                  ? textMsg(document, false)
+              // photo msg
+              // : document.data()['type'] == 1
+              //     ? photoMsg(document, context)
+              //     // file type message
+              //     : document.data()['type'] == 2
+              //         ? fileMsg(document)
+              // dummy else statement for error message type
+              // : Container(),
+            ],
+          ),
+          // timestamp(document, false),
+        ],
+      ),
+    );
+  }
+}
--- lib/chat/chatScreens/message_list.dart
+++ lib/chat/chatScreens/message_list.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:study_pal/chat/chatScreens/message.dart';
 
 class MessageList extends StatefulWidget {
   // final Staff peer;
@@ -36,24 +37,27 @@ class _MessageListState extends State<MessageList> {
     // final user = Provider.of<User>(context);
     // final String groupId = user.uid.hashCode <= widget.peer.firebaseId.hashCode ? user.uid + '-' + widget.peer.firebaseId : widget.peer.firebaseId + '-' + user.uid;
     final String groupId = "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1-sW7nZtffcdRXrZN9HMNF3FLSw692"; //temp
+
     // print(groupId);
     return Flexible(
       // child: Text(groupId),
       child: StreamBuilder(
-        stream: FirebaseFirestore.instance.collection('messages').doc(groupId).collection(groupId).orderBy('timestamp', descending: true).limit(_limit).snapshots(),
+        stream: FirebaseFirestore.instance.collection('message').doc(groupId).collection(groupId).orderBy('timestamp', descending: true).limit(_limit).snapshots(),
         builder: (context, snapshot) {
-          if (!snapshot.hasData) {
+          print(snapshot.data.docs.length);
+          if (snapshot.data.docs.length == 0) {
             return Center(child: Text("no message"));
           } else {
-            listMessage.addAll(snapshot.data.documents);
+            listMessage.addAll(snapshot.data.docs);
             return ListView.builder(
               //   scrollDirection: Axis.vertical,
               // shrinkWrap: true,
               padding: EdgeInsets.all(10.0),
-              // itemBuilder: (context, index) => message(context, index, snapshot.data.documents[index], user, widget.peer),
+              // itemBuilder: (context, index) => message(context, index, snapshot.data.docs[index], user, widget.peer),
+              itemBuilder: (context, index) => message(context, index, snapshot.data.docs[index], "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1", "sW7nZtffcdRXrZN9HMNF3FLSw692"),
               // itemBuilder: (context, index) => Text(index.toString() + snapshot.data.documents[index].toString()),
-              itemBuilder: (context, index) => Text(index.toString()),
-              itemCount: snapshot.data.documents.length,
+              // itemBuilder: (context, index) => Text(index.toString()),
+              itemCount: snapshot.data.docs.length,
               reverse: true,
               controller: listScrollController,
             );
