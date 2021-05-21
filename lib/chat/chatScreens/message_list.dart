d31e5451caf29ebbbd94852eb55ebb75dfc5882a
wylerwwl <wongwailun84@gmail.com>
2021-05-21 18:11:23 +0800

    trying to get user profile

--- lib/chat/chatScreens/message_list.dart
+++ lib/chat/chatScreens/message_list.dart
@@ -1,10 +1,10 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:study_pal/chat/chatScreens/message.dart';
 
 class MessageList extends StatefulWidget {
   // final Staff peer;
-
   // MessageList({this.peer});
 
   @override
@@ -19,9 +19,7 @@ class _MessageListState extends State<MessageList> {
   final ScrollController listScrollController = ScrollController();
 
   _scrollListener() {
-    if (listScrollController.offset >=
-            listScrollController.position.maxScrollExtent &&
-        !listScrollController.position.outOfRange) {
+    if (listScrollController.offset >= listScrollController.position.maxScrollExtent && !listScrollController.position.outOfRange) {
       setState(() {
         _limit += _limitIncrement;
       });
@@ -36,20 +34,13 @@ class _MessageListState extends State<MessageList> {
 
   @override
   Widget build(BuildContext context) {
-    // final user = Provider.of<User>(context);
-    // final String groupId = user.uid.hashCode <= widget.peer.firebaseId.hashCode ? user.uid + '-' + widget.peer.firebaseId : widget.peer.firebaseId + '-' + user.uid;
-    final String groupId =
-        "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1-sW7nZtffcdRXrZN9HMNF3FLSw692"; //temp
+    final String userId = FirebaseAuth.instance.currentUser!.uid;
+    // final String groupId = userId.hashCode <= widget.peer.firebaseId.hashCode ? user.uid + '-' + widget.peer.firebaseId : widget.peer.firebaseId + '-' + user.uid;
+    final String groupId = "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1-sW7nZtffcdRXrZN9HMNF3FLSw692"; //temp
 
     return Flexible(
       child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
-        stream: FirebaseFirestore.instance
-            .collection('message')
-            .doc(groupId)
-            .collection(groupId)
-            .orderBy('timestamp', descending: true)
-            .limit(_limit)
-            .snapshots(),
+        stream: FirebaseFirestore.instance.collection('message').doc(groupId).collection(groupId).orderBy('timestamp', descending: true).limit(_limit).snapshots(),
         builder: (context, snapshot) {
           try {
             if (snapshot.data!.docs.length == 0) {
@@ -59,12 +50,7 @@ class _MessageListState extends State<MessageList> {
               return ListView.builder(
                 padding: EdgeInsets.all(10.0),
                 // itemBuilder: (context, index) => message(context, index, snapshot.data.docs[index], user, widget.peer),
-                itemBuilder: (context, index) => message(
-                    context,
-                    index,
-                    snapshot.data!.docs[index],
-                    "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1",
-                    "sW7nZtffcdRXrZN9HMNF3FLSw692"),
+                itemBuilder: (context, index) => message(context, index, snapshot.data!.docs[index], "mwU8cQ1IX9gu6RXLNvF9AHJNYDm1", "sW7nZtffcdRXrZN9HMNF3FLSw692"),
                 // itemBuilder: (context, index) => Text(index.toString() + snapshot.data.documents[index].toString()),
 
                 itemCount: snapshot.data!.docs.length,
